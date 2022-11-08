7ebba1dbf6b4ca370a75285deb234ab192fa2b22
Bestpaul <bestpaulpipeloluwa@gmail.com>
2022-11-08 05:47:38 +0100

    jackpot actions

--- src/app/store/jackpots/action.ts
+++ src/app/store/jackpots/action.ts
@@ -1,8 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { Jackpot } from '../../interfaces';
 
-
-export const invokeJackpotAPI = createAction('[JACKPOT API] Invoke Jackpot Fetch API');
 export const loadJackpots = createAction('[Jackpots] Load jackpots into store', props<{ allJackpot: Jackpot[] }>());
 export const jackpotFetchAPISuccess = createAction('[JACKPOT API] Fetch API Success',props<{ allJackpot: Jackpot[] }>());
 export const loadJackpotsFailed = createAction('[Jackpots] Load jackpots failed');
